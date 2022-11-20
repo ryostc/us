b7e2e90cc204219510e76a4416fb3689d8205964
ryostc <ryo.316.stc@icloud.com>
2022-11-21 04:54:05 +0900

    add:マイグレーション
    instructorテーブルに入校日のカラムが設定されていなかったため追加

---
+++ database/migrations/2022_11_20_194502_add_enrollment_date_to_instructors_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('instructors', function (Blueprint $table) {
+            $table->datetime('enrollment_date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('instructors', function (Blueprint $table) {
+            $table->dropColumn('enrollment_date');
+        });
+    }
+};
