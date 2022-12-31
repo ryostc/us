9042c33e4bc2f670804652a39893a1f7ffcf1ffc
ryostc <ryo.316.stc@icloud.com>
2022-12-31 22:13:03 +0900

    add:マイグレーションの追加
    studentsテーブルの５つのカラムのdefaultで初期設定しているところをnullableに変更

---
+++ database/migrations/2022_12_31_130324_change_students_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('students', function (Blueprint $table) {
+            // データ型をnullableにする
+            $table->string("guardian_firstname")->nullable()->change();
+            $table->string("guardian_lastname")->nullable()->change();
+            $table->string("guardian_firstname_ruby")->nullable()->change();
+            $table->string("guardian_lastname_ruby")->nullable()->change();
+            $table->string("relationship")->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('students', function (Blueprint $table) {
+            // データ型をnullableでなくdefaultを設定した状態に戻す
+            $table->string("guardian_firstname")->default("")->change();
+            $table->string("guardian_lastname")->default("")->change();
+            $table->string("guardian_firstname_ruby")->default("")->change();
+            $table->string("guardian_lastname_ruby")->default("")->change();
+            $table->string("relationship")->default("")->change();
+        });
+    }
+};
