afbd1a18efe8b1e9aa6dc970ec1c394478e84ced
ryostc <ryo.316.stc@icloud.com>
2023-01-05 17:29:07 +0900

    add:マイグレーション作成
    studentsテーブルのenrollment_dateカラムをnullableに変更

---
+++ database/migrations/2023_01_05_082528_change_enrollment_date_to_students_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('students', function (Blueprint $table) {
+            // データ型をnullabelにする
+            $table->date("enrollment_date")->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('students', function (Blueprint $table) {
+            // データ型のnullableを解除する
+            $table->date("enrollment_date")->change();
+        });
+    }
+};
